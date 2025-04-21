d60ee58202d9c346e9c2eeb6a008385e7ea255ca
zhanwenxin22 <zhanwen.w.xin@gmail.com>
2025-04-21 18:08:50 -0400

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://zhanwen-xin.github.io',
+  base: 'zhanwen-xin.github.io',
+})
